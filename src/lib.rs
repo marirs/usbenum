3293e6bdda24909ee7651c692c5adcfb935a7436
Sriram G <marirs@gmail.com>
2021-08-23 11:17:53 +0530

    macos enumeration

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,18 @@ mod platform;
 #[cfg(target_os = "macos")]
 #[path = "platforms/macos.rs"]
 mod platform;
+
+pub fn list_connected_usb_drives() -> Result<Vec<ConnectedUsbDevices>> {
+    //! Get a list of currently connected USB drives
+    //! ## Example
+    //!
+    //! ```rust
+    //! use usbenum::list_connected_usb_drives;
+    //!
+    //! match list_connected_usb_drives() {
+    //!     Ok(l) => println!("{:?}", l),
+    //!     Err(e) => println!("Error: {:?}", e),
+    //! }
+    //! ```
+    platform::enumerate_connected_usb()
+}
